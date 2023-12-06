242cba87e11801de12ce3db09f48931e22b84d0d
Albert Tan Jian You <jianyou0214@gmail.com>
2023-12-06 14:39:01 +0800

    new

---
+++ app/src/main/java/com/example/fyp/Account.kt
@@ -0,0 +1,9 @@
+package com.example.fyp
+
+data class Account(
+    var accName : String = "",
+    var accCardNumber : Int = 0,
+    var accCardDate : Int = 0,
+    var accCardCode : Int = 0,
+    var accCardAmount : Double = 0.0
+)
