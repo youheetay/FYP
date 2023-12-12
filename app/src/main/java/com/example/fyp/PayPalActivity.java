e5bc61bfdb772b557e1f27ba44c180cc4ca645b5
Jason Tan Jian Hee <tanjianhee0214@gmail.com>
2023-12-12 22:25:36 +0800

    12/12 10.25pm

--- app/src/main/java/com/example/fyp/PayPalActivity.java
+++ app/src/main/java/com/example/fyp/PayPalActivity.java
@@ -1,91 +1,91 @@
-package com.example.fyp;
-
-import androidx.appcompat.app.AppCompatActivity;
-
-import android.os.Bundle;
-import android.util.Log;
-import android.widget.Toast;
-
-import com.paypal.checkout.approve.Approval;
-import com.paypal.checkout.approve.OnApprove;
-import com.paypal.checkout.createorder.CreateOrder;
-import com.paypal.checkout.createorder.CreateOrderActions;
-import com.paypal.checkout.createorder.CurrencyCode;
-import com.paypal.checkout.createorder.OrderIntent;
-import com.paypal.checkout.createorder.UserAction;
-import com.paypal.checkout.order.Amount;
-import com.paypal.checkout.order.AppContext;
-import com.paypal.checkout.order.CaptureOrderResult;
-import com.paypal.checkout.order.OnCaptureComplete;
-import com.paypal.checkout.order.OrderRequest;
-import com.paypal.checkout.order.PurchaseUnit;
-import com.paypal.checkout.paymentbutton.PaymentButtonContainer;
-
-import org.jetbrains.annotations.NotNull;
-
-import java.util.ArrayList;
-
-public class PayPalActivity extends AppCompatActivity {
-
-    private static final String TAG = "MyTag";
-    PaymentButtonContainer paymentButtonContainer;
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_pay_pal);
-
-        paymentButtonContainer = findViewById(R.id.payment_button_container);
-
-        try {
-            paymentButtonContainer.setup(
-                new CreateOrder() {
-                    @Override
-                    public void create(@NotNull CreateOrderActions createOrderActions) {
-                        Log.d(TAG, "create: ");
-                        ArrayList<PurchaseUnit> purchaseUnits = new ArrayList<>();
-                        purchaseUnits.add(
-                                new PurchaseUnit.Builder()
-                                        .amount(
-                                                new Amount.Builder()
-                                                        .currencyCode(CurrencyCode.USD)
-                                                        .value("10.00")
-                                                        .build()
-                                        )
-                                        .build()
-                        );
-                        OrderRequest order = new OrderRequest(
-                                OrderIntent.CAPTURE,
-                                new AppContext.Builder()
-                                        .userAction(UserAction.PAY_NOW)
-                                        .build(),
-                                purchaseUnits
-                        );
-                        createOrderActions.create(order, (CreateOrderActions.OnOrderCreated) null);
-                    }
-                },
-                new OnApprove() {
-                    @Override
-                    public void onApprove(@NotNull Approval approval) {
-                        approval.getOrderActions().capture(new OnCaptureComplete() {
-                            @Override
-                            public void onCaptureComplete(@NotNull CaptureOrderResult result) {
-                                Log.d(TAG, String.format("CaptureOrderResult: %s", result));
-                                Toast.makeText(PayPalActivity.this, "Successful", Toast.LENGTH_SHORT).show();
-                            }
-                        });
-                    }
-                }
-        );
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-
-    }
-
-    @Override
-    public void onBackPressed() {
-        super.onBackPressed();
-        finish();
-    }
-}
+//package com.example.fyp;
+//
+//import androidx.appcompat.app.AppCompatActivity;
+//
+//import android.os.Bundle;
+//import android.util.Log;
+//import android.widget.Toast;
+//
+//import com.paypal.checkout.approve.Approval;
+//import com.paypal.checkout.approve.OnApprove;
+//import com.paypal.checkout.createorder.CreateOrder;
+//import com.paypal.checkout.createorder.CreateOrderActions;
+//import com.paypal.checkout.createorder.CurrencyCode;
+//import com.paypal.checkout.createorder.OrderIntent;
+//import com.paypal.checkout.createorder.UserAction;
+//import com.paypal.checkout.order.Amount;
+//import com.paypal.checkout.order.AppContext;
+//import com.paypal.checkout.order.CaptureOrderResult;
+//import com.paypal.checkout.order.OnCaptureComplete;
+//import com.paypal.checkout.order.OrderRequest;
+//import com.paypal.checkout.order.PurchaseUnit;
+//import com.paypal.checkout.paymentbutton.PaymentButtonContainer;
+//
+//import org.jetbrains.annotations.NotNull;
+//
+//import java.util.ArrayList;
+//
+//public class PayPalActivity extends AppCompatActivity {
+//
+//    private static final String TAG = "MyTag";
+//    PaymentButtonContainer paymentButtonContainer;
+//
+//    @Override
+//    protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_pay_pal);
+//
+//        paymentButtonContainer = findViewById(R.id.payment_button_container);
+//
+//        try {
+//            paymentButtonContainer.setup(
+//                new CreateOrder() {
+//                    @Override
+//                    public void create(@NotNull CreateOrderActions createOrderActions) {
+//                        Log.d(TAG, "create: ");
+//                        ArrayList<PurchaseUnit> purchaseUnits = new ArrayList<>();
+//                        purchaseUnits.add(
+//                                new PurchaseUnit.Builder()
+//                                        .amount(
+//                                                new Amount.Builder()
+//                                                        .currencyCode(CurrencyCode.USD)
+//                                                        .value("10.00")
+//                                                        .build()
+//                                        )
+//                                        .build()
+//                        );
+//                        OrderRequest order = new OrderRequest(
+//                                OrderIntent.CAPTURE,
+//                                new AppContext.Builder()
+//                                        .userAction(UserAction.PAY_NOW)
+//                                        .build(),
+//                                purchaseUnits
+//                        );
+//                        createOrderActions.create(order, (CreateOrderActions.OnOrderCreated) null);
+//                    }
+//                },
+//                new OnApprove() {
+//                    @Override
+//                    public void onApprove(@NotNull Approval approval) {
+//                        approval.getOrderActions().capture(new OnCaptureComplete() {
+//                            @Override
+//                            public void onCaptureComplete(@NotNull CaptureOrderResult result) {
+//                                Log.d(TAG, String.format("CaptureOrderResult: %s", result));
+//                                Toast.makeText(PayPalActivity.this, "Successful", Toast.LENGTH_SHORT).show();
+//                            }
+//                        });
+//                    }
+//                }
+//        );
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//        }
+//
+//    }
+//
+//    @Override
+//    public void onBackPressed() {
+//        super.onBackPressed();
+//        finish();
+//    }
+//}
